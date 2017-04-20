b89a615517bdd34426e4f52755363ea035a682b9
Diana Vázquez <dianavazquez.dcg@gmail.com>
2017-04-19 21:25:56 -0500

    aparece ventana al dar click a imagen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
-function showFull(){
-    console.log(event);
-    var contenedor = document.getElementById("fullImage");
-    var imagen = document.getElementsByClassName("fotito");
+var ventana = document.getElementById("ventanaEmergente");
+var imagenVentana = document.getElementById("imagenVentana");
+var fotos = document.getElementsByClassName("fotito");
+
+var tache = document.getElementById("tache");
+
+function detectarImagen() {
+    mostrarVentana(this);
 }
+
+function mostrarVentana(imagen){
+    console.log(ventana);
+    ventana.style.display = "block";
+    imagenVentana.src = imagen.src;
+}
+
+for(var i = 0; i < fotos.length; i++){
+    fotos[i].addEventListener("click",detectarImagen);
+}
+
